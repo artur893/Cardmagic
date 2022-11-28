a1679602a17fa96036a058e7fc14a8b6a481ccd6
artur893 <arthal893@gmail.com>
2022-11-28 12:19:06 +0100

    Created basic 15 cards

--- src/components/cardsData.js
+++ src/components/cardsData.js
@@ -1,9 +1,109 @@
 const cards = [{
-    name: 'Garbaty Goblin',
+    name: 'Goblin',
     attack: 1,
     hp: 1,
     cost: 1,
-    skill: null
+    skill: null,
+    type: 'mob'
+},
+{
+    name: 'Ratlerek',
+    attack: 2,
+    hp: 1,
+    cost: 1,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Słomiany Rycerz',
+    attack: 1,
+    hp: 1,
+    cost: 1,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Garbaty Anioł',
+    attack: 2,
+    hp: 2,
+    cost: 2,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Karzeł Rambo',
+    attack: 3,
+    hp: 1,
+    cost: 2,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Bramkarz',
+    attack: 2,
+    hp: 3,
+    cost: 2,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Policjant',
+    attack: 3,
+    hp: 3,
+    cost: 3,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Moździerz',
+    attack: 1,
+    hp: 5,
+    cost: 3,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Chirurg',
+    attack: 3,
+    hp: 3,
+    cost: 3,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Operator SWAT',
+    attack: 4,
+    hp: 4,
+    cost: 4,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Diler',
+    attack: 2,
+    hp: 2,
+    cost: 4,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Ślepy snajper',
+    attack: 10,
+    hp: 4,
+    cost: 5,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Golem',
+    attack: 3,
+    hp: 9,
+    cost: 5,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Smok',
+    attack: 6,
+    hp: 6,
+    cost: 6,
+    skill: null,
+    type: 'mob'
+}, {
+    name: 'Bóg',
+    attack: 8,
+    hp: 8,
+    cost: 7,
+    skill: null,
+    type: 'mob'
 }]
 
 export { cards }

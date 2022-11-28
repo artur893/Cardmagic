879a547311198959ff97c75c9586bc7f7a44a330
artur893 <arthal893@gmail.com>
2022-11-28 08:48:21 +0100

    Created heroes

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import './App.css'
+import { Header } from "./components/Header";
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            heroes: [{
+                name: 'Wojownik',
+                hp: 30,
+                skillName: 'Szarża',
+                skillText: 'Zadaj 2pkt obrażeń',
+                skill: 'method here'
+            }, {
+                name: 'Czarodziej',
+                hp: 30,
+                skillName: 'Omam',
+                skillText: 'Przejmij kontrolę nad wrogim stronnikiem',
+                skill: 'method here'
+            }, {
+                name: 'Mnich',
+                hp: 30,
+                skillName: 'Medytuj',
+                skillText: 'Dodaj sobie +1 maksymalnej many',
+                skill: 'method here'
+            }, {
+                name: 'Wampir',
+                hp: 30,
+                skillName: 'Ugryź',
+                skillText: 'Zadaj 1pkt obrażeń, ulecz 1hp',
+                skill: 'method here'
+            }],
+            cards: [{
+                name: 'Garbaty Goblin',
+                attack: 1,
+                hp: 1,
+                cost: 1,
+                skill: null
+            }]
+        }
+    }
+
+    render() {
+        return <div className="game-container"><Header /></div>
+    }
+}
+
+export default App 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import './Header.css'
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                Cardmagic
+            </header>)
+    }
+}
+
+export { Header }
